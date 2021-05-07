6ab480103496db1e7d6578edff2cb2d9dcb0ceab
EC2 Default User <ec2-user@ip-172-31-40-246.us-east-2.compute.internal>
2021-05-07 09:04:03 +0000

    second-commit

---
+++ reserve_app/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :email, :image, :profile)
+  end
+  
+end
